bdc6fb957386cea37a621886b7cee00953dc3ca4
wilofice <wilofice@gmail.com>
2016-11-18 22:14:09 +0000

    Configuring WgChatBot to receive and send message

--- index.js
+++ index.js
@@ -57,6 +57,101 @@ app.post('/webhook', function(req, res){
 });
 
 function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+	 
+    //console.log("Message data ", event.message);
+    console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
 
-    console.log("Message data ", event.message);
+    var messageId = message.mid;
+
+    var messageText = message.text;
+    var messageAttachements = message.attachements;
+
+    if(messageText) {
+        switch (messageText) {
+	    case 'generic':
+	        sendGenericMessage(senderID, messageText);
+		break;
+	    default: 
+	        sendTextMessage(senderID, messageText);
+	}
+
+    } else if(messageAttachements) {
+    	sendTextMessage(senderID, "Message with attachement received");
+    }
 }
+
+function sendGenericMessage(senderID, messageText) {
+	
+}
+
+function sendTextMessage(recipientID, messageText){
+    var messageData = {
+      	recipient: {
+	    id: recipientId
+	},
+
+	message: {
+	    text: messageText
+	}
+	};
+
+    callSendAPI(messageData);
+}
+
+function callSendAPI(messageData){
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages', 
+	qs: {access_token: PAGE_ACCESS_TOKEN},
+	method: 'POST',
+	json: messageData
+	
+    }, function(error, response, body) {
+           if(!error && response.statusCode == 200) {
+	       var recipientId = body.recipient_id;
+	       var messageId = body.message_id;
+
+	       console.log("Successfully sent generic message with id %d to recipient %s", messageId, recipientId);
+	   } else {
+	   	console.error("Unable to send message.");
+		console.error(response);
+		console.error(error);
+	   }	
+    } 
+    
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
